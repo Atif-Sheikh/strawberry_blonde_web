83408200726aa78b798cafce5cebae45c1328faa
Muhammad Atif <atifsiddiquissg@gmail.com>
2019-08-02 15:41:33 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Navbar,
+  NavbarBrand,
+  TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col
+} from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    activeTab: 1
+  };
+
+  toggle = (tab) => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Strawberry Blonde</NavbarBrand>
+        </Navbar>
+        <div className="App-header">
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                onClick={() => { this.toggle('1'); }}
+              >
+                Add Product
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                onClick={() => { this.toggle('2'); }}
+              >
+                Best Colletions
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                onClick={() => { this.toggle('3'); }}
+              >
+                Women
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={this.state.activeTab}>
+            <TabPane tabId="1">
+              <Row>
+                <Col sm="12">
+                  <h4>Tab 1 Contents</h4>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <Row>
+                <Col sm="6">
+                  <Card body>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button>Go somewhere</Button>
+                  </Card>
+                </Col>
+                <Col sm="6">
+                  <Card body>
+                    <CardTitle>Special Title Treatment</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button>Go somewhere</Button>
+                  </Card>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="3">
+              <Row>
+                <Col sm="12">
+                  <h4>Tab 3 Contents</h4>
+                </Col>
+              </Row>
+            </TabPane>
+          </TabContent>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+var config = {
+    apiKey: "AIzaSyAUQF6YYuVuT1jdhwdVFmwCIthOG0f2io4",
+    authDomain: "campusrecuirtment.firebaseapp.com",
+    databaseURL: "https://strawberry-eca3e.firebaseio.com/",
+    projectId: "strawberry-eca3e",
+    storageBucket: "campusrecuirtment.appspot.com",
+    messagingSenderId: "874600307382"
+  };
+  firebase.initializeApp(config);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
